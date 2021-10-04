69f82de22c726c3387876e7209d0f8d73204a33c
Cristian Miranda <mirandadehoyos2003@gmail.com>
2021-10-04 10:05:34 -0500

    Usando find y arrays de objetos creamos la funcion de usar cupones y dependiendo de este damos el descuento total, añadiendo un ifelse y modificando el html para recibir los cupones

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -1,6 +1,22 @@
 // const precio = 100;
 // const descuento = 15;
 
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
+
+
 function calcularPrecioConDescuento (precio, descuento) {
 
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -15,12 +31,29 @@ function buttonPriceDiscount(){
     const inputPrice = document.getElementById("inputPrice");
     const price = inputPrice.value;
 
-    const inputDiscount = document.getElementById("inputDiscount");
-    const discount = inputDiscount.value;
+    const inputCoupons = document.getElementById("inputCoupons");
+    const couponDiscount = inputCoupons.value;
 
-    const PriceDiscount = calcularPrecioConDescuento(price, discount);
+    const couponsValueValid = function(coupon){
+        return coupon.name === couponDiscount;
+    }
 
-    const text = document.getElementById("priceP");
-    text.innerText = `El precio total de el producto en descuento es el de: ${PriceDiscount}`;
+    const userCoupon = coupons.find(couponsValueValid);
+
+    if (!userCoupon) {
+        alert("El cupon " + discount + " no es valido")
+    } else {
+
+        const discount = userCoupon.discount;
+        const PriceDiscount = calcularPrecioConDescuento(price, discount);
+        const total = price - PriceDiscount;
+
+        const text = document.getElementById("priceP");
+        text.innerText = `El precio total de el producto en descuento es el de: $${PriceDiscount}
+                          ahorraste en total: $${total}`;
+
+    }
 
 }
+
+
